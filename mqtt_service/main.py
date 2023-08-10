3c6866640b39d1726ef5c2eb7fbaf9d97e3b11eb
Ahmed Fahim <afahim03@yahoo.com>
2023-08-10 11:43:35 +0600

    Add call to main function

--- mqtt_service/main.py
+++ mqtt_service/main.py
@@ -41,25 +41,18 @@ def on_message(client, userdata, message):
     write_db(
         timestamp=parsed_message["timestamp"], moisture=int(parsed_message["moisture"])
     )
-    # cursor.execute(
-    #     f"INSERT into moistures (Times, moisture) values ('{parsed_message['timestamp']}', {parsed_message['moisture']})"
-    # )
-    # connection.commit()
-    print("Received message: ", str(message.payload.decode("utf-8")))
+    # print("Received message: ", str(message.payload.decode("utf-8")))
 
 
 def init_db():
     cursor.execute(
         "CREATE TABLE if not exists moistures (Times DATETIME NOT NULL PRIMARY KEY, moisture INT)"
     )
-    # return connection, cursor
 
 
 def main():
     client = mqtt.Client("MQTT_database_service")
-    client.username_pw_set(
-        username=username, password=password
-    )  # uncomment if you use password auth
+    client.username_pw_set(username=username, password=password)
     client.on_connect = on_connect
     client.on_message = on_message
 
@@ -67,14 +60,8 @@ def main():
 
     client.connect(broker_hostname, port)
 
-    # connection, cursor = init_db()
-
-    # connection = connect(
-    #     host=db_host, user=db_user, password=db_password, database=db_database
-    # )
-    # cursor = connection.cursor()
-    # cursor.execute(
-    #     "CREATE TABLE if not exists moistures (Times DATETIME, moisture INT)"
-    # )
-
     client.loop_forever()
+
+
+if __name__ == "__main__":
+    main()
